ef76a3f30d393ce4ed42038f479581ba61bc54de
George Kartavenko <SocialImp@mail.ru>
2025-10-22 20:43:37 +0300

    :add: backend init files

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+// Переменные окружения
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(helmet()); // ???
+app.use(cors());
+app.use(morgan('combined')); // ????
+app.use(express.json());
+
+// Роут для проеверки
+app.get('/api/health', (req, res) => {
+  res.json({
+    status: 'OK',
+    message: 'ContentGen Backend is running!',
+    timestamp: new Date().toISOString(),
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
