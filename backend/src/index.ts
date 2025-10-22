901a8edd598a988b72c84efa021fb8765556dc79
George Kartavenko <SocialImp@mail.ru>
2025-10-22 21:22:53 +0300

    :feat: comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,9 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(helmet()); // ???
+app.use(helmet()); // Безопасность
 app.use(cors());
-app.use(morgan('combined')); // ????
+app.use(morgan('combined')); // Логирование. Сейчас полное.
 app.use(express.json());
 
 // Роут для проеверки
